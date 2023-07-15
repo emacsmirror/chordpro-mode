63c51a86ef54bba09aebc9b1a01ad0a688e7c16d
Joseph Turner <joseph@breatheoutbreathe.in>
2023-07-15 16:48:03 -0700

    Change: Require Emacs 28.1
    compat.el does not support split-string-shell-command, but this
    functionality could be added into this package in order to backport
    chordpro-mode to pre-28.1 versions.

--- chordpro-mode.el
+++ chordpro-mode.el
@@ -3,7 +3,7 @@
 ;; Author: Howard Ding <hading2@gmail.com>
 ;; Author: Joseph Turner <public@breatheoutbreathe.in>
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "26.1") (compat "29.1.4.1"))
+;; Package-Requires: ((emacs "28.1") (compat "29.1.4.1"))
 ;; Homepage: https://git.sr.ht/~breatheoutbreathein/chordpro-mode.el/
 ;; Keywords: convenience
 
