bf6b01b69e7d22d464816afeb60f55403d8b3241
Joseph Turner <joseph@breatheoutbreathe.in>
2023-07-15 16:26:23 -0700

    Fix: Add Commentary section

--- chordpro-mode.el
+++ chordpro-mode.el
@@ -49,7 +49,10 @@
 
 ;;; Commentary:
 
-;;
+;; This major mode is for editing files in the ChordPro format:
+;; <https://www.chordpro.org/chordpro/chordpro-introduction/>
+
+;; Files with the .cho extension will automatically use `chordpro-mode'.
 
 ;;; Code:
 
